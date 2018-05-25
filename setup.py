367fc324b2107d2f973b0e63ed342fd1c623e826
Niklas Rosenstein <rosensteinniklas@gmail.com>
2018-05-25 12:39:31 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+
+import setuptools
+
+def readme():
+  with open('README.md') as fp:
+    return fp.read()
+
+def requirements():
+  with open('requirements.txt') as fp:
+    return fp.readlines()
+
+setuptools.setup(
+  name = 'vizardry',
+  author = 'Niklas Rosenstein',
+  author_email = 'rosensteinniklas@gmail.com',
+  long_description = readme(),
+  long_description_content_type = 'text/markdown',
+  install_requires = requirements(),
+  packages = setuptools.find_packages(),
+  entry_points = {
+    'console_scripts': [
+      'vizardry = vizardry.main:main'
+    ]
+  }
+)
